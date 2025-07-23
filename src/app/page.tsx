9e003939e618e47cf4c6eace8c1bafb65fac9234
Diego Perdomo <diegoperdomo94@gmail.com>
2025-07-23 17:04:06 -0500

    create database models

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <>Page</>;
+import db from "@/lib/db";
+
+export default async function Home() {
+  const user = await db.user.findFirst();
+
+  return (
+    <>
+      <h1>Hello from NextJS</h1>
+      <p>{JSON.stringify(user)}</p>
+    </>
+  );
 }
